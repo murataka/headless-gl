a004c2425540f49c991a24cfa262d17183bd13f5
murat aka <muratakaus@yahoo.com>
2022-06-10 11:01:02 +0300

    before angle upgrade

---
+++ useit.js
@@ -0,0 +1,25 @@
+// Create context
+var fs =require('fs');
+var width   = 64
+var height  = 64
+var gl = require('.')(width, height, { preserveDrawingBuffer: true })
+ 
+
+//Clear screen to red
+gl.clearColor(1, 0, 0, 1)
+gl.clear(gl.COLOR_BUFFER_BIT)
+
+//Write output as a PPM formatted image
+var pixels = new Uint8Array(width * height * 4)
+gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+
+//process.stdout.write(['P3\n# gl.ppm\n', width, " ", height, '\n255\n'].join(''))
+fs.appendFileSync("yummy.ppm",['P3\n# gl.ppm\n', width, " ", height, '\n255\n'].join(''));
+
+for(var i = 0; i < pixels.length; i += 4) {
+  for(var j = 0; j < 3; ++j) {
+  //  process.stdout.write(pixels[i + j] + ' ');
+    fs.appendFileSync("yummy.ppm",pixels[i + j] + ' ');
+    
+  }
+}
